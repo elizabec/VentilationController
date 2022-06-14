4ea6efb38c7466cf76bf71e477042e0892eae59e
Elizabeth Crockford-Härkönen <ecrockford@gmail.com>
2022-06-14 13:06:00 +0300

    bug fixes in button commands

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
 
-RUN apt-get -y update && apt-get -y install git && apt-get install tk -y && apt-get install numpy
+RUN apt-get -y update && apt-get -y install git && apt-get install tk -y
 
 RUN git clone https://github.com/elizabec/VentilationController.git
 
--- robot_gui.py
+++ robot_gui.py
@@ -24,86 +24,115 @@ class RobotGUI(tk.Tk):
             
 
         self.show_frame("StartPage")
+        self.print_output("START")
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
         
-
     def print_output(self, output):
-        pass #TODO: Add in green/red bg colour for RF pass/fail
+        if output == "PASS":
+            self.configure(bg='green')
+        elif output == "FAIL":
+            self.configure(bg='red')
+        elif output == "START":
+            self.configure(bg='blue')
 
     def run_robot_read(self, control):
         if control == "USB":
             subprocess.run(["robot", "-i", "read", "vent_tests_USB.robot"])
+            #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "read", "vent_tests_USB.robot"])
         elif control == "MQTT":
             subprocess.run(["robot", "-i", "read", "vent_tests_MQTT.robot"])
+            #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "read", "vent_tests_MQTT.robot"])
 
     def run_robot_write(self, control, mode):
         if control == "USB":
             if mode == "auto":
                 subprocess.run(["robot", "-i", "pressure", "vent_tests_USB.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "pressure", "vent_tests_USB.robot"])
             elif mode == "manual":
                 subprocess.run(["robot", "-i", "speed", "vent_tests_USB.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "speed", "vent_tests_USB.robot"])
         elif control == "MQTT":
             if mode == "auto":
                 subprocess.run(["robot", "-i", "pressure", "vent_tests_MQTT.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "pressure", "vent_tests_MQTT.robot"])
             elif mode == "manual":
                 subprocess.run(["robot", "-i", "speed", "vent_tests_MQTT.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "speed", "vent_tests_MQTT.robot"])
 
     def run_robot_nodip(self, control, mode):
         if control == "USB":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "nodips", "-e", "manual", "vent_tests_USB.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "nodips", "-e", "manual", "vent_tests_USB.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "nodips", "-e", "automatic", "vent_tests_USB.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "nodips", "-e", "automatic", "vent_tests_USB.robot"])
         elif control == "MQTT":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "nodips", "-e", "manual", "vent_tests_MQTT.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "nodips", "-e", "manual", "vent_tests_MQTT.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "nodips", "-e", "automatic", "vent_tests_MQTT.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "nodips", "-e", "automatic", "vent_tests_MQTT.robot"])
 
     def run_robot_dip1(self, control, mode):
         if control == "USB":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "dipone", "-e", "manual", "vent_tests_USB.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "dipone", "-e", "manual", "vent_tests_USB.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "dipone", "-e", "automatic", "vent_tests_USB.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "dipone", "-e", "automatic", "vent_tests_USB.robot"])
         elif control == "MQTT":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "dipone", "-e", "manual", "vent_tests_MQTT.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "dipone", "-e", "manual", "vent_tests_MQTT.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "dipone", "-e", "automatic", "vent_tests_MQTT.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "dipone", "-e", "automatic", "vent_tests_MQTT.robot"])
 
     def run_robot_dip2(self, control, mode):
         if control == "USB":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "diptwo", "-e", "manual", "vent_tests_USB.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "diptwo", "-e", "manual", "vent_tests_USB.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "diptwo", "-e", "automatic", "vent_tests_USB.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "diptwo", "-e", "automatic", "vent_tests_USB.robot"])
         elif control == "MQTT":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "diptwo", "-e", "manual", "vent_tests_MQTT.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "diptwo", "-e", "manual", "vent_tests_MQTT.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "diptwo", "-e", "automatic", "vent_tests_MQTT.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "diptwo", "-e", "automatic", "vent_tests_MQTT.robot"])
 
     def run_robot_bothdip(self, control, mode):
         if control == "USB":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "bothdip", "-e", "manual", "vent_tests_USB.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "bothdip", "-e", "manual", "vent_tests_USB.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "bothdip", "-e", "automatic", "vent_tests_USB.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "bothdip", "-e", "automatic", "vent_tests_USB.robot"])
         elif control == "MQTT":
-            if mode == "auto":
+            if mode == "manual":
                 subprocess.run(["robot", "-i", "bothdip", "-e", "manual", "vent_tests_MQTT.robot"])
-            elif mode == "manual":
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "bothdip", "-e", "manual", "vent_tests_MQTT.robot"])
+            elif mode == "auto":
                 subprocess.run(["robot", "-i", "bothdip", "-e", "automatic", "vent_tests_MQTT.robot"])
+                #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "bothdip", "-e", "automatic", "vent_tests_MQTT.robot"])
 
     def run_robot_error(self, control):
         if control == "USB":
             subprocess.run(["robot", "-i", "errorflag", "vent_tests_USB.robot"])
+            #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "errorflag", "vent_tests_USB.robot"])
         elif control == "MQTT":
             subprocess.run(["robot", "-i", "errorflag", "vent_tests_MQTT.robot"])
+            #subprocess.run(["docker", "exec", "robot-docker-pi", "robot", "-i", "errorflag", "vent_tests_MQTT.robot"])
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -184,7 +213,7 @@ class USBManSelect(tk.Frame):
         button4 = tk.Button(self, text="Vents: Dip Switch 1", command=lambda: controller.run_robot_dip1("USB", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
         button5 = tk.Button(self, text="Vents: Dip Switch 2", command=lambda: controller.run_robot_dip2("USB", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
         button6 = tk.Button(self, text="Vents: Both Dip Switches", command=lambda: controller.run_robot_bothdip("USB", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
-        button7 = tk.Button(self, text="Check Error Flag", command=lambda: controller.run_robot_error("USB", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
+        button7 = tk.Button(self, text="Check Error Flag", command=lambda: controller.run_robot_error("USB"), height=1, width=20, font=("Arial", 20, "bold"))
         button8 = tk.Button(self, text="Return to Start", command=lambda: controller.show_frame("StartPage"), height=1, width=15, font=("Arial", 20, "bold"))
 
         button1.pack()
@@ -232,7 +261,7 @@ class MQTTManSelect(tk.Frame):
         button4 = tk.Button(self, text="Vents: Dip Switch 1", command=lambda: controller.run_robot_dip1("MQTT", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
         button5 = tk.Button(self, text="Vents: Dip Switch 2", command=lambda: controller.run_robot_dip2("MQTT", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
         button6 = tk.Button(self, text="Vents: Both Dip Switches", command=lambda: controller.run_robot_bothdip("MQTT", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
-        button7 = tk.Button(self, text="Check Error Flag", command=lambda: controller.run_robot_error("MQTT", "manual"), height=1, width=20, font=("Arial", 20, "bold"))
+        button7 = tk.Button(self, text="Check Error Flag", command=lambda: controller.run_robot_error("MQTT"), height=1, width=20, font=("Arial", 20, "bold"))
         button8 = tk.Button(self, text="Return to Start", command=lambda: controller.show_frame("StartPage"), height=1, width=15, font=("Arial", 20, "bold"))
 
         button1.pack()
